4124b5a87a46e05ff1aa24dd33e71f5e057081b5
RobinUrcun <107709383+RobinUrcun@users.noreply.github.com>
2026-06-07 00:33:25 +0200

    changement du nom du parametre pour aller sur la page nos livres

--- Views/home.php
+++ Views/home.php
@@ -9,7 +9,7 @@ include_once(__DIR__ . "/../Layout/header.php");
         <p class="homepage_description">
             Donnez une nouvelle vie à vos livres en les échangeant avec d'autres amoureux de la lecture. Nous croyons en la magie du partage de connaissances et d'histoires à travers les livres.
         </p>
-        <a href="index.php?page=our_books" class="main_button">
+        <a href="index.php?page=nos_livres" class="main_button">
             Découvrir
         </a>
     </div>
@@ -59,7 +59,7 @@ include_once(__DIR__ . "/../Layout/header.php");
             </div>
         </div>
     </div>
-    <a href="index.php?page=our_books" class="main_button">
+    <a href="index.php?page=nos_livres" class="main_button">
         Voir tous les livres
     </a>
 </section>
@@ -85,7 +85,7 @@ include_once(__DIR__ . "/../Layout/header.php");
             <p>Proposez un échange et discutez avec d'autres passionnés de lecture.</p>
         </div>
     </div>
-    <a href="index.php?page=our_books" class="main_button reverse_button">
+    <a href="index.php?page=nos_livres" class="main_button reverse_button">
         Voir tous les livres
     </a>
 </section>
