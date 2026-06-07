c2868a2fdf1ec6d3638e16e5912f8156bb8ccc67
RobinUrcun <107709383+RobinUrcun@users.noreply.github.com>
2026-06-07 05:31:05 +0200

    Fix : renommage des erreurs

--- Utils/FormValidation.php
+++ Utils/FormValidation.php
@@ -8,7 +8,7 @@ class FormValidation
         if (!$pseudo) {
             throw new Exception("Le pseudo est requis");
         } elseif (strlen($pseudo) < 5) {
-            throw new Exception("Le pseudo est doit faire minimum 5 caractères");
+            throw new Exception("Le pseudo doit faire minimum 5 caractères");
         }
     }
 
@@ -26,7 +26,7 @@ class FormValidation
         if (!$password) {
             throw new Exception("Le mot de passse est requis");
         } elseif (strlen($password) < 8) {
-            throw new Exception("Le mot de passse est doit faire minimum 8 caractères");
+            throw new Exception("Le mot de passe  doit faire minimum 8 caractères");
         }
     }
 
@@ -55,5 +55,46 @@ class FormValidation
     public static function isFileSizeCorrect(int $fileSize): bool
     {
         return $fileSize < 5242880;
+    }
+
+    public static function isTitleValid(?string $title)
+    {
+        if (!$title) {
+            throw new Exception("Le titre est requis");
+        } elseif (strlen($title) < 5) {
+            throw new Exception("Le titre doit faire minimum 5 caractères");
+        }
+    }
+
+    public static function isAuthorValid(?string $author)
+    {
+        if (!$author) {
+            throw new Exception("L'auteur est requis");
+        } elseif (strlen($author) < 5) {
+            throw new Exception("L'auteur doit faire minimum 5 caractères");
+        }
+    }
+
+    public static function isCommentValid(?string $comment)
+    {
+        if (!$comment) {
+            throw new Exception("Le commentaire est requis");
+        } elseif (strlen($comment) < 5) {
+            throw new Exception("Le commentaire  doit faire minimum 5 caractères");
+        }
+    }
+
+    public static function isDisponibilityValid(?array $disponibility)
+    {
+
+        $disponibilityMapping = [
+            "available",
+            "unavailable"
+        ];
+        if (!$disponibility) {
+            throw new Exception("La disponibilité est requise");
+        } elseif (array_key_exists($disponibility, $disponibilityMapping)) {
+            throw new Exception("Ce type de disponibilité n'existe pas");
+        }
     }
 }
