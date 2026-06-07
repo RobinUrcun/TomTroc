c16f5d53fe645e6d0399fc54a2ea476198c85728
RobinUrcun <107709383+RobinUrcun@users.noreply.github.com>
2026-06-07 06:13:34 +0200

    Ajout du model Book

---
+++ Models/Book.php
@@ -0,0 +1,86 @@
+<?php
+
+class Book
+{
+
+    private string $id;
+    private string $title;
+    private string $author;
+    private string $comment;
+    private string $disponibility;
+    private string $image_file_name;
+    private DateTime $created_at;
+
+
+    public function __construct() {}
+
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    public function setId(int $id)
+    {
+        $this->id = $id;
+    }
+
+    public function getTitle()
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title)
+    {
+        $this->title = $title;
+    }
+
+    public function getAuthor()
+    {
+        return $this->author;
+    }
+
+    public function setAuthor(string $author)
+    {
+        $this->author = $author;
+    }
+
+    public function getComment()
+    {
+        return $this->comment;
+    }
+
+    public function setComment(string $comment)
+    {
+        $this->comment = $comment;
+    }
+
+    public function getDisponibility()
+    {
+        return $this->disponibility;
+    }
+
+    public function setDisponibility(string $disponibility)
+    {
+        $this->disponibility = $disponibility;
+    }
+
+    public function getImageFileName()
+    {
+        return $this->image_file_name;
+    }
+
+    public function setImageFileName(string $image_file_name)
+    {
+        $this->image_file_name = $image_file_name;
+    }
+
+    public function getCreatedAt()
+    {
+        return $this->created_at;
+    }
+
+    public function setCreatedAt(DateTime $created_at)
+    {
+        $this->created_at = $created_at;
+    }
+}
