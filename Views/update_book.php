7c2008ca156947818fa28ea56680a482e0cf5457
RobinUrcun <107709383+RobinUrcun@users.noreply.github.com>
2026-06-11 17:18:23 +0200

    Affichage des erreurs sur le formulaire d'upload d'image

--- Views/update_book.php
+++ Views/update_book.php
@@ -16,14 +16,22 @@ include_once(__DIR__ . "/../Layout/header.php");
     <div class="form_wrapper">
         <form enctype="multipart/form-data" action="./index.php?page=modifier_un_livre&book_id=<?= isset($book) ? $book->getId() : "" ?>" method="POST">
             <div class="image_upload_wrapper">
-                <label for="book_picture">
+                <h2 for="book_picture">
                     Photo
-                </label>
+                </h2>
                 <div class="image_wrapper">
                     <img src="Public/Uploads/Books/<?= isset($book) ? $book->getImageFileName() : 'default_book_image.webp' ?>" alt="">
                 </div>
 
-                <input id="book_picture" name="book_picture" type="file">
+                <label class="file_uploader_label" for="book_picture">
+                    Modifier la photo
+                </label>
+
+                <input class="file_uploader_input<?= isset($bookErrorManager) && $bookErrorManager->isFileError ? 'error' : ''  ?>" id="book_picture" name="book_picture" type="file">
+
+                <div class="auth_input_error_message <?= isset($bookErrorManager) && $bookErrorManager->isFileError ? 'active' : ''  ?>">
+                    <?= isset($bookErrorManager) ? $bookErrorManager->fileErrorMessage : null  ?>
+                </div>
             </div>
             <div class="informations_upload_wrapper">
 
