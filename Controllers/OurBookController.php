4eb4680ae00b672132f276e9665fee2d7c7809d7
RobinUrcun <107709383+RobinUrcun@users.noreply.github.com>
2026-06-14 05:32:03 +0200

    remplacement de l'affichage statique des livres par l'affichage dynamique

--- Controllers/OurBookController.php
+++ Controllers/OurBookController.php
@@ -6,6 +6,9 @@ class OurBookController
     public function getOurBookPage()
     {
         $user = AuthServices::getAuthenticatedUser();
+        $bookRepository = new BookRepository();
+
+        $books = $bookRepository->get();
 
         $title = "Nos livres à l'échange";
         require_once("./Views/our_book.php");
--- Views/our_book.php
+++ Views/our_book.php
@@ -1,7 +1,6 @@
 <?php
 
 include_once(__DIR__ . "/../Layout/header.php");
-
 ?>
 
 <section class="our_books_section">
@@ -19,102 +18,20 @@ include_once(__DIR__ . "/../Layout/header.php");
     </div>
 
     <div class="display_all_books">
-        <div class="card">
-            <img src="./Public/Assets/to_delete/3464b64a922f7d911d69633167d3700d8c0b3049.jpg" alt="">
-            <div class="card_content">
-                <h2 class="card_title">Esther</h2>
-                <p class="card_author">Alabaster</p>
-                <p class="card_sold_by">Vendu par : CamilleClubLit</p>
-            </div>
-        </div>
-        <div class="card">
-            <img src="./Public/Assets/to_delete/3464b64a922f7d911d69633167d3700d8c0b3049.jpg" alt="">
-            <div class="card_content">
-                <h2 class="card_title">Esther</h2>
-                <p class="card_author">Alabaster</p>
-                <p class="card_sold_by">Vendu par : CamilleClubLit</p>
-            </div>
-        </div>
-        <div class="card">
-            <img src="./Public/Assets/to_delete/3464b64a922f7d911d69633167d3700d8c0b3049.jpg" alt="">
-            <div class="card_content">
-                <h2 class="card_title">Esther</h2>
-                <p class="card_author">Alabaster</p>
-                <p class="card_sold_by">Vendu par : CamilleClubLit</p>
-            </div>
-        </div>
-        <div class="card">
-            <img src="./Public/Assets/to_delete/3464b64a922f7d911d69633167d3700d8c0b3049.jpg" alt="">
-            <div class="card_content">
-                <h2 class="card_title">Esther</h2>
-                <p class="card_author">Alabaster</p>
-                <p class="card_sold_by">Vendu par : CamilleClubLit</p>
-            </div>
-        </div>
-        <div class="card">
-            <img src="./Public/Assets/to_delete/3464b64a922f7d911d69633167d3700d8c0b3049.jpg" alt="">
-            <div class="card_content">
-                <h2 class="card_title">Esther</h2>
-                <p class="card_author">Alabaster</p>
-                <p class="card_sold_by">Vendu par : CamilleClubLit</p>
-            </div>
-        </div>
-        <div class="card">
-            <img src="./Public/Assets/to_delete/3464b64a922f7d911d69633167d3700d8c0b3049.jpg" alt="">
-            <div class="card_content">
-                <h2 class="card_title">Esther</h2>
-                <p class="card_author">Alabaster</p>
-                <p class="card_sold_by">Vendu par : CamilleClubLit</p>
-            </div>
-        </div>
-        <div class="card">
-            <img src="./Public/Assets/to_delete/3464b64a922f7d911d69633167d3700d8c0b3049.jpg" alt="">
-            <div class="card_content">
-                <h2 class="card_title">Esther</h2>
-                <p class="card_author">Alabaster</p>
-                <p class="card_sold_by">Vendu par : CamilleClubLit</p>
-            </div>
-        </div>
-        <div class="card">
-            <img src="./Public/Assets/to_delete/3464b64a922f7d911d69633167d3700d8c0b3049.jpg" alt="">
-            <div class="card_content">
-                <h2 class="card_title">Esther</h2>
-                <p class="card_author">Alabaster</p>
-                <p class="card_sold_by">Vendu par : CamilleClubLit</p>
-            </div>
-        </div>
-        <div class="card">
-            <img src="./Public/Assets/to_delete/3464b64a922f7d911d69633167d3700d8c0b3049.jpg" alt="">
-            <div class="card_content">
-                <h2 class="card_title">Esther</h2>
-                <p class="card_author">Alabaster</p>
-                <p class="card_sold_by">Vendu par : CamilleClubLit</p>
-            </div>
-        </div>
-        <div class="card">
-            <img src="./Public/Assets/to_delete/3464b64a922f7d911d69633167d3700d8c0b3049.jpg" alt="">
-            <div class="card_content">
-                <h2 class="card_title">Esther</h2>
-                <p class="card_author">Alabaster</p>
-                <p class="card_sold_by">Vendu par : CamilleClubLit</p>
-            </div>
-        </div>
-        <div class="card">
-            <img src="./Public/Assets/to_delete/3464b64a922f7d911d69633167d3700d8c0b3049.jpg" alt="">
-            <div class="card_content">
-                <h2 class="card_title">Esther</h2>
-                <p class="card_author">Alabaster</p>
-                <p class="card_sold_by">Vendu par : CamilleClubLit</p>
-            </div>
-        </div>
-        <div class="card">
-            <img src="./Public/Assets/to_delete/3464b64a922f7d911d69633167d3700d8c0b3049.jpg" alt="">
-            <div class="card_content">
-                <h2 class="card_title">Esther</h2>
-                <p class="card_author">Alabaster</p>
-                <p class="card_sold_by">Vendu par : CamilleClubLit</p>
-            </div>
-        </div>
+        <?php if (isset($books) && count($books)) : ?>
+
+            <?php foreach ($books as $book) :  ?>
+                <div class="card">
+                    <img src="./Public/Uploads/Books/<?= $book->getImageFileName() ?>" alt="">
+                    <div class="card_content">
+                        <h2 class="card_title"><?= $book->getTitle() ?></h2>
+                        <p class="card_author"><?= $book->getAuthor() ?></p>
+                        <p class="card_sold_by">Vendu par : <?= $book->getUser()->getPseudo() ?></p>
+                    </div>
+                </div>
+            <?php endforeach;  ?>
+
+        <?php endif; ?>
 
     </div>
 </section>
